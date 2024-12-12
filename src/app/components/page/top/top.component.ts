4fb48022a8714b82867c2d4bd978958a02454d13
mkixine <kkato@diverta.co.jp>
2024-12-12 21:47:36 +0900

    wip

--- src/app/components/page/top/top.component.ts
+++ src/app/components/page/top/top.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../../section/top/hero/hero.component';
 import { Ranking } from '../../common/ranking/ranking.component';
 import { CardList } from '../../ui/card-list/card-list.component';
+import { HtmlPartsComponent } from '../../common/html-parts/html-parts.component';
 
 @Component({
   selector: 'app-top',
-  imports: [Hero, Ranking, CardList],
+  imports: [Hero, Ranking, CardList, HtmlPartsComponent],
   templateUrl: './top.component.html',
   styleUrl: './top.component.scss',
 })
